979bd6c080f1acdee1329efd722098532509bb7f
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-06-23 22:37:47 +0530

    Develop jwt token expire function and session time out

--- client/src/components/userDetails.js
+++ client/src/components/userDetails.js
@@ -24,6 +24,11 @@ export default class UserDetails extends Component {
             .then((data) => {
               console.log(data, "userData");
               this.setState({ userData: data.data});
+              if(data.data=='token expired'){
+                alert("Token expired login again");
+                window.localStorage.clear();
+                window.location.href = "./sign-in";
+              }
             });
     }
     logOut = () => {
--- server/app.js
+++ server/app.js
@@ -61,7 +61,9 @@ app.post("/login-user",async(req,res)=>{
     return res.json({ error: "User Not Found" });
   }
   if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({email:user.email}, JWT_SECRET);
+    const token = jwt.sign({email:user.email}, JWT_SECRET, {
+      expiresIn: 10,
+    });
     if (res.status(201)){
       return res.json({ status: "ok", data: token});
     }else{
@@ -74,8 +76,16 @@ app.post("/login-user",async(req,res)=>{
 app.post("/userData", async (req, res) => {
   const { token } = req.body;
   try{
-    const user=jwt.verify(token,JWT_SECRET);
+    const user=jwt.verify(token,JWT_SECRET, (err, res) => {
+      if (err) {
+        return "token expired";
+      }
+      return res;
+    });
     console.log(user);
+    if(user=="token expired"){
+      return res.send({ status: "error", data: "token expired"});
+    }
     const usermail = user.email;
     User.findOne({ email: usermail}).collation({})
     .then((data) => {
